52ccc488540055759e52cea07a9794d82d77a689
PetruLucan <lucanpetru11@gmail.com>
2020-08-27 14:16:02 +0300

    Updates

---
+++ precache-manifest.9fd7cbcd500ecb36c7f3f965947ec54f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a70aae22a7dbde727a2bad7596367a9f",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "0b376b6f34e4ca03ae4f",
+    "url": "/monster-rolodex/static/css/main.8377ad72.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/monster-rolodex/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b376b6f34e4ca03ae4f",
+    "url": "/monster-rolodex/static/js/main.1f457a28.chunk.js"
+  },
+  {
+    "revision": "212570b3a5f291f8b8f3",
+    "url": "/monster-rolodex/static/js/runtime-main.4a41f6c3.js"
+  }
+]);
